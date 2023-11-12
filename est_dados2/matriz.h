fb0564f00623dd53751127685caba03ff69cc0b3
Alice Cruz <127883364+aliceczr@users.noreply.github.com>
2023-11-12 19:44:12 -0300

    Update matriz.h

--- est_dados2/matriz.h
+++ est_dados2/matriz.h
@@ -1,15 +1,14 @@
 #ifndef MATRIZ_H
 #define MATRIZ_H
+#include <stdio.h>
 
 typedef struct Celula {
     struct Celula *direita, *abaixo;
     int linha, coluna;
     float valor;
-    struct Celula* antecessor;
-    struct Celula* sucessor;
 } Celula;
 
-typedef struct {
+typedef struct Matriz {
     Celula *cabeca;
     int nlin, ncol;
 } Matriz;
@@ -20,9 +19,13 @@ typedef struct ListaDuplamenteEncadeada {
     int coluna;
 } ListaDuplamenteEncadeada;
 
-void insere(Matriz *A, int i, int j, float v);
-Matriz leMatriz();
+
+void inicializaMatriz(struct Matriz* matriz, int nlin, int ncol);
+void insereCelula(ListaDuplamenteEncadeada *lista, int linha, int coluna, float valor);
+Matriz leMatriz(FILE *file);
 void imprimeMatriz(Matriz A);
 Matriz somaMatrizes(Matriz A, Matriz B);
 Matriz multiplicaMatrizes(Matriz A, Matriz B);
+Celula *buscaCelula(Matriz *mat, int linha, int coluna);
+
 #endif
